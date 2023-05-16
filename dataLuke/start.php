b65e3368371d509c5411e0edba30d795a54295f3
John42506176Linux <johnkongtcheu@gmail.com>
2023-05-16 06:13:46 -0700

    Editing Start RunId

---
+++ dataLuke/start.php
@@ -0,0 +1,34 @@
+<?php
+    header('Content-Type: application/json');
+    // Create a database connection
+    $servername = "localhost";
+    $username = "root";
+    $password = "Isagi11*";
+	$dbname = "CarData";
+    $conn = new mysqli($servername, $username, $password, $dbname);
+
+    // Check connection
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
+
+        $sql = "SELECT max(runID) FROM pi_table";
+        $result = $conn->query($sql);
+        $data = array();
+        // Check if any rows were returned
+        if ($result->num_rows > 0) {
+
+            // Output each row of data
+            while($row = $result->fetch_assoc()) {
+                $data[] = $row;
+            }
+            $result->close();
+            // Output the table footer
+            print json_encode($data);
+        } 
+
+
+        // Close the database connection
+        $conn->close();
+
+    ?>
--- delete.php
+++ delete.php
@@ -5,14 +5,14 @@
     $username = "root";
     $password = "Isagi11*";
     $dbname = "test";
-	$dbname = "Weather";
+	$dbname = "CarData";
 	$conn = new mysqli($servername, $username, $password, $dbname);
 
 	// Check connection
 	if ($conn->connect_error) {
 	    die("Connection failed: " . $conn->connect_error);
 	}
-    $sql = "DELETE FROM weather_table;";
+    $sql = "DELETE FROM pi_table;";
     $conn->query($sql);
     print json_encode("Success");
 
